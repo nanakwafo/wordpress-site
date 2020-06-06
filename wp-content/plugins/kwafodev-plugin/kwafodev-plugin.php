a2bb1a7bfdfeb8fb9f3a673b08018b7565c21ce6
Nana Mensah <nanamensah@robs-mbp.home>
2020-06-06 18:43:28 +0100

    plugin step 1

--- wp-content/plugins/kwafodev-plugin/kwafodev-plugin.php
+++ wp-content/plugins/kwafodev-plugin/kwafodev-plugin.php
@@ -13,3 +13,58 @@
  License:GPLv2
  Text Domain: kwafodev-plugin
  */
+
+if(! defined('ABSPATH')){
+    die;
+}
+
+
+/**
+ * Class KwafodevPlugin
+ */
+class KwafodevPlugin
+{
+
+
+    /**
+     * KwafodevPlugin constructor.
+     */
+    public function __construct()
+    {
+        add_action('init',array($this,'custom_post_type'));
+    }
+
+    /**
+     * activate method
+     */
+    public function activate(){
+        $this->custom_post_type();
+        flush_rewrite_rules();
+    }
+
+    /**
+     *deactivation method
+     */
+    public function deactivate(){
+        $this->custom_post_type();
+        flush_rewrite_rules();
+    }
+
+
+   public function custom_post_type(){
+        register_post_type('book',['public' =>true, 'label'=>'Books']);
+   }
+}
+
+//instantiate class
+if( class_exists('KwafodevPlugin')){
+    $kwafodevPlugin = new KwafodevPlugin();
+}
+
+
+//activation
+register_activation_hook(__FILE__, array($kwafodevPlugin,'activate'));
+//deactivation
+register_deactivation_hook(__FILE__, array($kwafodevPlugin,'deactivate'));
+//uninstall
+
---
+++ wp-content/plugins/kwafodev-plugin/uninstall.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package MysitePlugin
+ */
+if( ! defined('WP_UNINSTALL_PLUGIN') ){
+    die;
+}
+
+$books = get_posts(array('post_type'=>'book','numberposts'=> -1));
+foreach ($books as $book){
+    wp_delete_post($book->ID,true);
+}
