e70577703b114a82a08244c0fdd5bbbed3a8e528
Nana Mensah <nanamensah@robs-mbp.home>
2020-06-06 17:32:59 +0100

    wordpress to recognise plugin

---
+++ wp-content/plugins/kwafodev-plugin/kwafodev-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package MysitePlugin
+ */
+
+/*
+ Plugin Name: kwafodev-plugin
+ Plugin URI: https://kwafonanadev.co.uk/
+ Description: This is my first attempt in creating custom plugin in wordpress
+ Version: 1.0.0
+ Author: Kwafo nana Mensah
+ Aurthor URI: https://kwafonanadev.co.uk/
+ License:GPLv2
+ Text Domain: kwafodev-plugin
+ */
